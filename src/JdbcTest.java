817c1f410010107f06e9a8b762f913dc38715f99
ajay15081999 <akykumar2020as@gmail.com>
2024-10-03 00:16:58 +0530

    Use JDBC with sql

---
+++ src/JdbcTest.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+
+
+
+public class JdbcTest {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_records", "root" , "Aj@9631294779");
+			
+			System.out.println("Database connection successful!\n");
+			
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL query
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			// 4. Process the result set
+			while (myRs.next()) {
+				//System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name")+", "+myRs.getString("email"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+	}
+
+}
